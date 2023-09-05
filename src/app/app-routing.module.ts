d075a19d19f56aad2d98d7020de492d5eb90e274
Bárbara Damasceno <115722516+barbaradamasdev@users.noreply.github.com>
2023-09-05 08:27:20 -0300

    add pathMatch to routes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   {
     path:'',
-    component: HomeComponent
+    component: HomeComponent,
+    pathMatch: 'full'
   },
   {
     path:'content/:id',
